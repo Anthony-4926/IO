03ea64bdbc4ba8f0e86089a4b3e2c4161394ab7a
zhishoumuguinian <2936616693@qq.com>
2019-03-10 11:57:12 +0800

    两种流模式&try-with-resource

--- src/com/zhishoumuguinian/ioTest.java
+++ src/com/zhishoumuguinian/ioTest.java
@@ -1,6 +1,8 @@
 package com.zhishoumuguinian;
 
 import java.io.*;
+import java.lang.reflect.Array;
+import java.time.LocalTime;
 
 /**
  * @author 赵鑫
@@ -11,28 +13,45 @@ public class ioTest {
         /**
          * 基于单个字符从文件中读取数据
          */
-        FileInputStream in = null;
-        FileOutputStream out = null;
+
         int c;
-        try {
-            in = new FileInputStream("C:\\Users\\赵鑫\\Desktop\\io.txt");
-            out = new FileOutputStream("C:\\Users\\赵鑫\\Desktop\\o.txt");
-            while ((c = in.read()) != -1){
-                System.out.println(c);
+        /**
+         * 将资源卸载try语句里，基于try-with-resource 自动关闭资源，简化了finally的资源关闭，
+         */
+        try (FileInputStream in = new FileInputStream("C:\\Users\\赵鑫\\Desktop\\io.txt");
+             FileOutputStream out = new FileOutputStream("C:\\Users\\赵鑫\\Desktop\\o.txt")) {
+
+            long begin = System.nanoTime();
+//             read()返回值为下一个流中下一个字符的assic码
+            while ((c = in.read()) != -1) {
+//                System.out.println(c);
+//                write()将assic码转换为字符写到文件中
                 out.write(c);
             }
+            long end = System.nanoTime();
+            System.out.println("第一种时间：" + (end - begin));
 
         } catch (FileNotFoundException e) {
             e.printStackTrace();
-        } catch (IOException e) {
+        }
+
+        /**
+         * 基于缓冲区读取文件
+         */
+
+        try (FileInputStream in = new FileInputStream("C:\\Users\\赵鑫\\Desktop\\io.txt");
+             FileOutputStream out = new FileOutputStream("C:\\Users\\赵鑫\\Desktop\\o1.txt")) {
+            byte[] buffer = new byte[3];
+            int len;
+            long begin = System.nanoTime();
+//            read(byte[]) 返回值为读取的长度
+            while ((len = in.read(buffer)) != -1) {
+                out.write(buffer, 0, len);
+            }
+            long end = System.nanoTime();
+            System.out.println("第二种时间：" + (end - begin));
+        } catch (FileNotFoundException e) {
             e.printStackTrace();
-        }finally {
-            if(in != null){
-                in.close();
-            }
-            if(out != null){
-                out.close();
-            }
         }
 
     }
